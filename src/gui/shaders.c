723802b994f9b5925bd92a18a642c0764c3bc503
Jack Bradach <jack@bradach.net>
2017-03-16 19:27:57 -0700

    - Reworking shader compiler/linker. - Unbroke the shader.

--- src/gui/shaders.c
+++ src/gui/shaders.c
@@ -4,8 +4,80 @@
 
 #include "file_utils.h"
 
+
+void _gl_perror(void)
+{
+    GLenum err = glGetError();
+
+    while(GL_NO_ERROR != err) {
+        const char *err_str;
+        switch(err) {
+        case GL_INVALID_OPERATION:
+            err_str = "INVALID_OPERATION";
+            break;
+        case GL_INVALID_ENUM:
+            err_str = "INVALID_ENUM";
+            break;
+        case GL_INVALID_VALUE:
+            err_str = "INVALID_VALUE";
+            break;
+        case GL_OUT_OF_MEMORY:
+            err_str = "OUT_OF_MEMORY";
+            break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            err_str = "INVALID_FRAMEBUFFER_OPERATION";
+            break;
+        }
+        perror(err_str);
+        err = glGetError();
+    }
+}
+
+int _glReportShaderCompileStatus(GLint shader)
+{
+    GLint compiled;
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+
+    if (!compiled) {
+        int len;
+        GLchar *info;
+
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+
+        info = calloc(len + 1, sizeof(GLchar));
+        glGetShaderInfoLog(shader, len, &len, info);
+        fprintf(stderr, "ShaderInfoLog: %s\n", info);
+        free(info);
+    }
+    return compiled;
+}
+
+int _glReportProgramLinkStatus(GLint program)
+{
+    GLint linked;
+
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+
+    if (!linked) {
+        int log_len;
+        GLchar *info;
+
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_len);
+
+        info = calloc(log_len + 1, sizeof(GLchar));
+        glGetProgramInfoLog(program, log_len, &log_len, info);
+        fprintf(stderr, "ProgramInfoLog: %s\n", info);
+        free(info);
+    } else {
+        fprintf(stderr, "Shader link success!\n");
+    }
+    return linked;
+}
+
 GLint shader_compile(const GLchar *src, GLint src_len, GLenum type);
 
+//TODO: rename to 'compile_shader_from_path'
 GLint shader_load_path(const char *path, GLenum type)
 {
     GLint shader;
@@ -29,26 +101,24 @@ GLint shader_load_path(const char *path, GLenum type)
 GLint shader_compile(const GLchar *src, GLint src_len, GLenum type)
 {
     const GLchar *shader_srcs[] = {src};
-    GLint shader;
-    GLint compiled;
-    shader = glCreateShader(type);
+    GLint shader = glCreateShader(type);
 
     glShaderSource(shader, 1, shader_srcs, &src_len);
     glCompileShader(shader);
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
 
-    if (!compiled) {
-        return 0;
+    if (_glReportShaderCompileStatus(shader)) {
+        return shader;
     }
 
-    return shader;
+    return 0;
 }
+
+// TODO: write a compile checkstatus.
 
 GLint shader_compile_program(const char *vertex_src, const char *fragment_src)
 {
     GLuint vertex, fragment;
-    GLuint program = 0;
-    GLint linked;
+    GLuint program;
 
     program = glCreateProgram();
     if (!program) {
@@ -71,18 +141,8 @@ GLint shader_compile_program(const char *vertex_src, const char *fragment_src)
     glAttachShader(program, vertex);
     glAttachShader(program, fragment);
     glLinkProgram(program);
-    glGetShaderiv(program, GL_LINK_STATUS, &linked);
-    if (!linked) {
-        int loglen;
-        fprintf(stderr, "Failed to link shader program:\n");
-        glGetShaderiv(program, GL_INFO_LOG_LENGTH, &loglen);
-        if (loglen > 0) {
-            char *info = (char *) malloc(loglen);
-            glGetShaderInfoLog(program, loglen, NULL, info);
-            free(info);
-        }
-        return 0;
-    }
+
+    _glReportProgramLinkStatus(program);
 
     glDetachShader(program, vertex);
     glDetachShader(program, fragment);
--- src/gui/shaders/test.vert
+++ src/gui/shaders/test.vert
@@ -1,7 +1,8 @@
-varying vec2 vTexCoord;
+#version 130
+varying vec2 txt_xy;
 
 void main()
 {
-    vTexCoord = gl_MultiTexCoord0;
+    txt_xy = gl_MultiTexCoord0.xy;
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
